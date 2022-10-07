57ee024556b5c831d873633eabb60db8d4695760
Artem Shinkaruk <artasend@gmail.com>
2022-10-07 22:23:58 +0700

    Make list head and tail private

--- src/homework-1/linked-list/ConcreteDoublyLinkedList.ts
+++ src/homework-1/linked-list/ConcreteDoublyLinkedList.ts
@@ -1,29 +1,37 @@
 import DoublyLinkedList, { DoublyLinkedListItem } from './DoublyLinkedList';
 
 export default class ConcreteDoublyLinkedList<T> implements DoublyLinkedList<T>, Iterable<T> {
-  head: DoublyLinkedListItem<T> | null = null;
+  #head: DoublyLinkedListItem<T> | null = null;
 
-  tail: DoublyLinkedListItem<T> | null = null;
+  #tail: DoublyLinkedListItem<T> | null = null;
+
+  get head() {
+    return this.#head;
+  }
+
+  get tail() {
+    return this.#tail;
+  }
 
   add(data: T): this {
     const item = {
       data,
-      prev: this.tail,
+      prev: this.#tail,
       next: null,
     };
-    if (this.head === null) {
-      this.head = item;
+    if (this.#head === null) {
+      this.#head = item;
     }
-    if (this.tail !== null) {
-      this.tail.next = item;
+    if (this.#tail !== null) {
+      this.#tail.next = item;
     }
-    this.tail = item;
+    this.#tail = item;
 
     return this;
   }
 
   * values(reverse: boolean = false): Iterator<T> {
-    let item = reverse ? this.tail : this.head;
+    let item = reverse ? this.#tail : this.#head;
     while (item) {
       yield item.data;
       item = reverse ? item.prev : item.next;
