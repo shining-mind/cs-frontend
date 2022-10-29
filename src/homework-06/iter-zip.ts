96ff53cfe998312010bc3e49d3789e116496c358
Artem Shinkaruk <artasend@gmail.com>
2022-10-29 14:30:49 +0700

    Fix done condition in iterZip

--- src/homework-06/iter-zip.ts
+++ src/homework-06/iter-zip.ts
@@ -9,10 +9,13 @@ export default function iterZip(...iterables: Iterable<any>[]): IterableIterator
     },
     next() {
       const tuple = new Array(iterators.length);
+      let allDone = true;
       for (let i = 0; i < iterators.length; i += 1) {
-        tuple[i] = iterators[i].next().value;
+        const { value, done } = iterators[i].next();
+        tuple[i] = value;
+        allDone = allDone && done;
       }
-      if (tuple.every((x) => x === undefined)) {
+      if (allDone) {
         return { value: undefined, done: true };
       }
       return { value: tuple, done: false };
